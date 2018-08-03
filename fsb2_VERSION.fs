f0500872b0f491af90545b46c471673f806d6f49
Marcos Cruz <github@programandala.net>
2018-08-03 12:51:18 +0200

    Update README

--- fsb2_VERSION.fs
+++ fsb2_VERSION.fs
@@ -3,5 +3,5 @@
 \ This file is part of fsb2
 \ http://programandala.net/en.program.fsb2.html
 
-: fsb2-version ( -- ca len ) s" 1.3.0+201707261653" ;
+: fsb2-version ( -- ca len ) s" 1.3.0+201808031249" ;
   \ Version after Semantic Versioning (http://semver.org).
