d0cef76a30bb6ee41c0637d125370c4a2fa28451
Marcos Cruz <github@programandala.net>
2020-02-16 02:12:21 +0100

    Release version 1.3.1

--- fsb2_VERSION.fs
+++ fsb2_VERSION.fs
@@ -3,5 +3,5 @@
 \ This file is part of fsb2
 \ http://programandala.net/en.program.fsb2.html
 
-: fsb2-version ( -- ca len ) s" 1.3.1-rc.0+202002160115" ;
+: fsb2-version ( -- ca len ) s" 1.3.1+20200216" ;
   \ Version after Semantic Versioning (http://semver.org).
